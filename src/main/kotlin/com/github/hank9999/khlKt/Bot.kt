a0b99b4cba217ac6ae84efce0c9d3f6edb343ccc
hank9999 <hank9999@qq.com>
2022-06-20 01:10:56 +0800

    refactor: change package path

---
+++ src/main/kotlin/com/github/hank9999/khlKt/Bot.kt
@@ -0,0 +1,39 @@
+package com.github.hank9999.khlKt
+
+import com.github.hank9999.khlKt.http.HttpApi
+import com.github.hank9999.khlKt.types.KhlEvent
+import com.github.hank9999.khlKt.types.KhlMessage
+import com.github.hank9999.khlKt.types.types.EventTypes
+import com.github.hank9999.khlKt.types.types.MessageTypes
+
+class Bot(config: Config) {
+    companion object {
+        lateinit var config: Config
+        lateinit var httpApi: HttpApi
+        lateinit var khlHandler: KhlHandler
+    }
+    init {
+        Companion.config = config
+        httpApi = HttpApi()
+        khlHandler = KhlHandler()
+        WebHook().initialize()
+    }
+
+    fun <T : Any> registerClass(t: T) {
+        khlHandler.registerClassHandler(t)
+    }
+
+    fun registerMessageFunc(type: MessageTypes, func: (msg: KhlMessage) -> Unit) {
+        khlHandler.registerMessageFuncHandler(type, func)
+    }
+
+    fun registerEventFunc(type: EventTypes, func: (event: KhlEvent) -> Unit) {
+        khlHandler.registerEventFuncHandler(type, func)
+    }
+
+    @Target(AnnotationTarget.FUNCTION)
+    annotation class OnMessage(val type: MessageTypes)
+
+    @Target(AnnotationTarget.FUNCTION)
+    annotation class OnEvent(val type: EventTypes)
+}
