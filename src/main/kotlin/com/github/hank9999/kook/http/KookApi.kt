536aa5676d5fd8a713ee9185571e853fa2c390da
hank9999 <hank9999@qq.com>
2022-09-13 00:20:34 +0800

    fix: avoid creating new instances repeatedly

--- src/main/kotlin/com/github/hank9999/kook/http/KookApi.kt
+++ src/main/kotlin/com/github/hank9999/kook/http/KookApi.kt
@@ -3,27 +3,39 @@ package com.github.hank9999.kook.http
 import com.github.hank9999.kook.http.kookapis.*
 
 class KookApi(private val httpApi: HttpApi) {
+    val message = Message(httpApi)
+    val guildRole = GuildRole(httpApi)
+    val user = User(httpApi)
+    val guild = Guild(httpApi)
+    val gateway = Gateway(httpApi)
+    val channelUser = ChannelUser(httpApi)
+    val channel = Channel(httpApi)
+
     fun Message(): Message {
-        return Message(httpApi)
+        return message
     }
 
     fun GuildRole(): GuildRole {
-        return GuildRole(httpApi)
+        return guildRole
     }
 
     fun User(): User {
-        return User(httpApi)
+        return user
     }
 
     fun Guild(): Guild {
-        return Guild(httpApi)
+        return guild
     }
 
     fun Gateway(): Gateway {
-        return Gateway(httpApi)
+        return gateway
     }
 
     fun ChannelUser(): ChannelUser {
-        return ChannelUser(httpApi)
+        return channelUser
+    }
+
+    fun Channel(): Channel {
+        return channel
     }
 }
