9dcf3e9bdd847ef8e88c7e5a11599464da384117
hank9999 <hank9999@qq.com>
2022-09-26 17:35:40 +0800

    fix: duration is double

--- src/main/kotlin/com/github/hank9999/kook/types/Attachments.kt
+++ src/main/kotlin/com/github/hank9999/kook/types/Attachments.kt
@@ -11,7 +11,7 @@ data class Attachments(
     @Required val name: String = "",
     @SerialName("file_type") val fileType: String = "",
     val size: Int = 0,
-    val duration: Int = 0,
+    val duration: Double = 0.0,
     val width: Int = 0,
     val height: Int = 0
 )
