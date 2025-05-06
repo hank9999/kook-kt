4937c020e56f627ce21cac20054fd945aeb7049e
hank9999 <hank9999@qq.com>
2025-05-07 03:58:57 +0800

    fix(GuildUser): add annotations for unknown fields in permission_users user mapping

--- src/main/kotlin/com/github/hank9999/kook/common/entity/user/GuildUser.kt
+++ src/main/kotlin/com/github/hank9999/kook/common/entity/user/GuildUser.kt
@@ -22,11 +22,18 @@ data class GuildUser(
     override val bot: Boolean,
     override val roles: List<Int>,
     @SerialName("is_sys") override val isSys: Boolean,
+    // nameplate 未知用途 List类型 内部类型未知 仅在permission_users的user中
 
     // --- Guild 特有字段 ---
     @SerialName("mobile_verified") val mobileVerified: Boolean,
     @SerialName("joined_at") val joinedAt: Long,
     @SerialName("active_time") val activeTime: Long,
+    // decorations_id_map kv类型 未知用途 仅在permission_users的user中, 下为该字段中已知的字段
+    // "join_voice": Int,
+    // "avatar_border": Int,
+    // "background": Int,
+    // "nameplate": Int,
+    // "nameplates": List<Int>
 
     // --- Bot 特有字段 (设为可空) ---
     @SerialName("bot_status") val botStatus: Optional<Int> = Optional.Missing(),
