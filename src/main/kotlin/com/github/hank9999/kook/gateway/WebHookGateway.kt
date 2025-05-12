ca6ffe036ee77b45969648f7a14b4df4f77446fd
hank9999 <hank9999@qq.com>
2025-05-13 05:06:13 +0800

    feat(gateway): add webhook initialization with routing, compression, and encryption options

--- src/main/kotlin/com/github/hank9999/kook/gateway/WebHookGateway.kt
+++ src/main/kotlin/com/github/hank9999/kook/gateway/WebHookGateway.kt
@@ -1,20 +1,65 @@
 package com.github.hank9999.kook.gateway
 
+import com.github.hank9999.kook.common.utils.CryptUtils
+import com.github.hank9999.kook.common.utils.JSON
+import com.github.hank9999.kook.common.utils.Zlib
+import com.github.hank9999.kook.common.utils.JSON.String
+import com.github.hank9999.kook.common.utils.JSON.get
+import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.server.engine.ApplicationEngine
-import kotlinx.coroutines.CoroutineScope
+import io.ktor.server.engine.EmbeddedServer
+import io.ktor.server.request.receiveStream
+import io.ktor.server.routing.post
+import io.ktor.server.routing.routing
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlin.coroutines.CoroutineContext
 
 class WebHookGateway : IGateway {
-    constructor(engine: ApplicationEngine, dispatcher: CoroutineContext, events: MutableSharedFlow<Event>) {
-        this.engine = engine
+    private val logger = KotlinLogging.logger {}
+
+    constructor(
+        dispatcher: CoroutineContext,
+        events: MutableSharedFlow<Event>,
+        server: EmbeddedServer<ApplicationEngine, ApplicationEngine.Configuration>,
+        path: String = "/kook-kt/webhook",
+        compress: Boolean = true,
+        verifyToken: String = "",
+        encryptKey: String = ""
+    ) {
         this.coroutineContext = SupervisorJob() + dispatcher
         this.events = events
+        this.server = server
+        this.path = path
+        this.compress = compress
+        this.verifyToken = verifyToken
+        this.encryptKey = encryptKey
     }
 
-    private val engine: ApplicationEngine
+    private val server: EmbeddedServer<ApplicationEngine, ApplicationEngine.Configuration>
     override val coroutineContext: CoroutineContext
     override val events: SharedFlow<Event>
+    private val path: String
+    private val compress: Boolean
+    private val verifyToken: String
+    private val encryptKey: String
+    val json = JSON.defaultJson()
+
+    fun initWebhook() {
+        server.application.routing {
+            post(path) {
+                val byteContent = call.receiveStream().readBytes()
+                val textContent = if (compress) Zlib.decompress(byteContent) else byteContent.decodeToString()
+                val rawElement = json.parseToJsonElement(textContent)
+                val element = if (encryptKey.isNotEmpty()) {
+                    val decryptJson = CryptUtils.decrypt(rawElement["encrypt"].String, encryptKey)
+                    json.parseToJsonElement(decryptJson)
+                } else {
+                    rawElement
+                }
+
+            }
+        }
+    }
 }
