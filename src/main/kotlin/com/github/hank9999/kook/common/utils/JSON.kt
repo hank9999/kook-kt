ff22ad1bc98687485d1a1c4a78f4a929420be48c
hank9999 <hank9999@qq.com>
2025-05-13 00:22:22 +0800

    refactor(utils): improve JSON extension properties and operators

--- src/main/kotlin/com/github/hank9999/kook/common/utils/JSON.kt
+++ src/main/kotlin/com/github/hank9999/kook/common/utils/JSON.kt
@@ -1,18 +1,46 @@
 package com.github.hank9999.kook.common.utils
 
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.boolean
+import kotlinx.serialization.json.double
+import kotlinx.serialization.json.float
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.long
 
-class JSON {
-    companion object {
-        private val json = Json {
-            encodeDefaults = false
-            ignoreUnknownKeys = true
-            isLenient = true
-            coerceInputValues = true
-        }
+object JSON {
+    operator fun JsonElement.get(i: String): JsonElement {
+        return jsonObject[i]!!
+    }
 
-        fun defaultJson(): Json {
-            return json
-        }
+    val JsonElement.String: String
+        get() { return this.jsonPrimitive.content }
+
+    val JsonElement.Int: Int
+        get() { return this.jsonPrimitive.int }
+
+    val JsonElement.Bool: Boolean
+        get() { return this.jsonPrimitive.boolean }
+
+    val JsonElement.Long: Long
+        get() { return this.jsonPrimitive.long }
+
+    val JsonElement.Double: Double
+        get() { return this.jsonPrimitive.double }
+
+    val JsonElement.Float: Float
+        get() { return this.jsonPrimitive.float }
+
+    private val json = Json {
+        encodeDefaults = false
+        ignoreUnknownKeys = true
+        isLenient = true
+        coerceInputValues = true
+    }
+
+    fun defaultJson(): Json {
+        return json
     }
 }
