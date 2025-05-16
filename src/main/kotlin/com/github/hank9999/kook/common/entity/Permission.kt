31584c0b1326b7bb724b0d07ac79840a842e4711
hank9999 <hank9999@qq.com>
2025-05-17 07:58:42 +0800

    fix(common): add custom serializers for permission interfaces

--- src/main/kotlin/com/github/hank9999/kook/common/entity/Permission.kt
+++ src/main/kotlin/com/github/hank9999/kook/common/entity/Permission.kt
@@ -1,15 +1,20 @@
 package com.github.hank9999.kook.common.entity
 
-import com.github.hank9999.kook.common.serializers.UserSerializer
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
+@Serializable(with = IPermissionOverwriteSerializer::class) // <--- 修改这里
 sealed interface IPermissionOverwrite {
     val roleId: Int
     val allow: Int
     val deny: Int
 }
 
+@Serializable(with = IPermissionUserSerializer::class) // <--- 修改这里
 sealed interface IPermissionUser {
     val user: IUser
     val userId: String
@@ -26,8 +31,20 @@ data class PermissionOverwrite(
 
 @Serializable
 data class PermissionUser(
-    @Serializable(with = UserSerializer::class) override val user: IUser,
+    override val user: IUser,
     @SerialName("user_id") override val userId: String,
     override val allow: Int,
     override val deny: Int
 ) : IPermissionUser
+
+internal object IPermissionOverwriteSerializer : KSerializer<IPermissionOverwrite> {
+    override val descriptor: SerialDescriptor = PermissionOverwrite.serializer().descriptor
+    override fun serialize(encoder: Encoder, value: IPermissionOverwrite) = encoder.encodeSerializableValue(PermissionOverwrite.serializer(), value as PermissionOverwrite)
+    override fun deserialize(decoder: Decoder): IPermissionOverwrite = decoder.decodeSerializableValue(PermissionOverwrite.serializer())
+}
+
+internal object IPermissionUserSerializer : KSerializer<IPermissionUser> {
+    override val descriptor: SerialDescriptor = PermissionUser.serializer().descriptor
+    override fun serialize(encoder: Encoder, value: IPermissionUser) = encoder.encodeSerializableValue(PermissionUser.serializer(), value as PermissionUser)
+    override fun deserialize(decoder: Decoder): IPermissionUser = decoder.decodeSerializableValue(PermissionUser.serializer())
+}
