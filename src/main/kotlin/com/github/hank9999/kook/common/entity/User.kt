2e61d36af75377414926e73c221023a13b399b0b
hank9999 <hank9999@qq.com>
2025-05-17 07:58:56 +0800

    refactor: move UserSerializer to User.kt and annotate IUser with @Serializable

--- src/main/kotlin/com/github/hank9999/kook/common/entity/User.kt
+++ src/main/kotlin/com/github/hank9999/kook/common/entity/User.kt
@@ -1,8 +1,19 @@
 package com.github.hank9999.kook.common.entity
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.JsonDecoder
+import kotlinx.serialization.json.booleanOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.longOrNull
 
+@Serializable(with = UserSerializer::class)
 sealed interface IUser {
     val id: String
     val username: String
@@ -133,3 +144,32 @@ data class GuildBotUser(
     @SerialName("client_id") val clientId: String,
     val verified: Boolean,
 ) : IGuildUser
+
+internal object UserSerializer : KSerializer<IUser> {
+    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("com.github.hank9999.kook.common.interfaces.IUser")
+
+    override fun serialize(encoder: Encoder, value: IUser) {
+        when (value) {
+            is User -> encoder.encodeSerializableValue(User.serializer(), value)
+            is SelfUser -> encoder.encodeSerializableValue(SelfUser.serializer(), value)
+            is GuildUser -> encoder.encodeSerializableValue(GuildUser.serializer(), value)
+            is GuildBotUser -> encoder.encodeSerializableValue(GuildBotUser.serializer(), value)
+        }
+    }
+
+    override fun deserialize(decoder: Decoder): IUser {
+        val jsonDecoder = decoder as JsonDecoder
+        val jsonElement = jsonDecoder.decodeJsonElement()
+        val jsonObject = jsonElement.jsonObject
+        val isBot = jsonObject["bot"]?.jsonPrimitive?.booleanOrNull ?: false
+        val isGuild = jsonObject["joined_at"]?.jsonPrimitive?.longOrNull != null
+        val isSelf = jsonObject["bot_status"]?.jsonPrimitive?.booleanOrNull ?: false
+
+        return when {
+            isSelf -> jsonDecoder.json.decodeFromJsonElement(SelfUser.serializer(), jsonElement)
+            isGuild && isBot -> jsonDecoder.json.decodeFromJsonElement(GuildBotUser.serializer(), jsonElement)
+            isGuild -> jsonDecoder.json.decodeFromJsonElement(GuildUser.serializer(), jsonElement)
+            else -> jsonDecoder.json.decodeFromJsonElement(User.serializer(), jsonElement)
+        }
+    }
+}
