ebaae3d763d874ec240a14c59846119eb1ab5566
hank9999 <hank9999@qq.com>
2025-05-18 06:54:13 +0800

    refactor(common): rename enum constants for clarity

--- src/main/kotlin/com/github/hank9999/kook/common/entity/MessageType.kt
+++ src/main/kotlin/com/github/hank9999/kook/common/entity/MessageType.kt
@@ -13,14 +13,14 @@ import kotlinx.serialization.encoding.Encoder
 enum class MessageType(val type: Int) {
     @Transient UNKNOWN(-1),
     TEXT(1),
-    IMG(2),
+    IMAGE(2),
     VIDEO(3),
     FILE(4),
     AUDIO(8),
     KMD(9),
     CARD(10),
     ITEM(12),
-    SYS(255);
+    SYSTEM(255);
 
     companion object {
         fun fromValue(value: Int): MessageType = entries.firstOrNull { it.type == value } ?: UNKNOWN
