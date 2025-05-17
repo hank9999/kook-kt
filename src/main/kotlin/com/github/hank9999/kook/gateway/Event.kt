4ac21ef6dd83b81bbc9b5e9ba180776f914542e4
hank9999 <hank9999@qq.com>
2025-05-17 08:00:28 +0800

    feat(gateway): add deserialization strategy skeleton for Event

--- src/main/kotlin/com/github/hank9999/kook/gateway/Event.kt
+++ src/main/kotlin/com/github/hank9999/kook/gateway/Event.kt
@@ -1,4 +1,23 @@
 package com.github.hank9999.kook.gateway
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
+import kotlinx.serialization.descriptors.nullable
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.DeserializationStrategy as KDeserializationStrategy
 
 sealed class Event {
+    object DeserializationStrategy : KDeserializationStrategy<Event> {
+        override val descriptor: SerialDescriptor = buildClassSerialDescriptor("com.github.hank9999.kook.gateway.Event") {
+            element("s", SignalType.serializer().descriptor)
+            element("sn", Int.serializer().descriptor.nullable)
+            element("d", JsonElement.serializer().descriptor.nullable)
+        }
+
+        override fun deserialize(decoder: Decoder): Event {
+            TODO("Not yet implemented")
+        }
+
+    }
 }
